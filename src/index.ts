a5531f6d3234089291b8bf3105658092539eb65a
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2021-01-19 02:00:50 +0900

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Format } from 'logform';
 import { PrettyConsoleFormatFactory, PrettyConsoleOptions } from './PrettyConsoleFormatFactory';
 
-function prettyConsole(levels: { [k: string]: number }, module?: NodeModule, options?: PrettyConsoleOptions): Format {
+function prettyConsole(
+  levels: { [k: string]: number } = { verbose: 1 },
+  module?: NodeModule,
+  options?: PrettyConsoleOptions
+): Format {
   return new PrettyConsoleFormatFactory(levels, options).create(module);
 }
 
